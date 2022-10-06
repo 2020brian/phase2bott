b98d3011fd8879d9256a7f52f10fac2c18aa3e88
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-10-07 00:21:53 +0300

    debugging

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,48 +1,18 @@
-import React, { useState, useEffect } from "react";
-import YourBotArmy from "./YourBotArmy";
-import BotCollection from "./BotCollection";
+import React from 'react';
+import BotCard from './BotCard';
 
-function BotsPage() {
-  //start here with your code for step one
-  const [bots, setBots] = useState([]);
+function BotCollection({ botList, addBotToArmy, deleteBot }) {
+  
+  const mappedBots = botList.map((bot) => {
+    return <BotCard bot={bot} key={bot.id} handleAdd={addBotToArmy} handleDelete={deleteBot}/>;
+  });
 
-  //a utility function to fetch data from the server
-  function fetchData() {
-    return fetch(`http://localhost:8002/bots`)
-      .then((resp) => resp.json())
-      .then((data) => {
-        setBots(data);
-      });
-  }
-  //run fetch whenever the page loads
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  //add bot to army when the bot is clicked
-
-  function enlistBot(bot) {
-    setBots(bots.map((b) => (b.id === bot.id ? { ...b, army: true } : b)));
-  }
-
-  function removeBot(bot) {
-    setBots(bots.map((b) => (b.id === bot.id ? { ...b, army: false } : b)));
-  }
-
-  function deleteBot(bot) {
-    const deletedBot = bots.filter((b) => b.id !== bot.id);
-    setBots((bots) => deletedBot);
-  }
   return (
-    <div>
-      <YourBotArmy
-        bots={bots.filter((b) => b.army)}
-        removeBot={removeBot}
-        deleteBot={deleteBot}
-      />
-      <BotCollection bots={bots} enlistBot={enlistBot} deleteBot={deleteBot} />
+    <div className='ui four column grid'>
+      Collection of all bots
+      <div className='row'>{mappedBots}</div>
     </div>
   );
 }
 
-export default BotsPage;
+export default BotCollection;
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,25 +1,27 @@
-import React from "react";
-import BotCard from "./BotCard";
+import React from 'react';
+import BotCard from './BotCard';
 
-function YourBotArmy({ bots, removeBot, deleteBot }) {
-  //your bot army code here...
-  const armyItem = bots.map((bot) => {
+function YourBotArmy({ botArmy, deleteBot, removeBotFromArmy, addBotToArmy }) {
+  console.log(botArmy);
+  //your bot army ..
+  const mappedBotArmy = botArmy.map((bot) => {
     return (
       <BotCard
         key={bot.id}
         bot={bot}
-        clickEvent={removeBot}
-        deleteBot={deleteBot}
+        handleAdd={removeBotFromArmy}
+        handleDelete={deleteBot}
       />
     );
   });
-
   return (
-    <div className="ui segment inverted olive bot-army">
-      <div className="ui five column grid">
-        <div className="row bot-army-row">
+    <div className='ui segment inverted olive bot-army'>
+      <div className='ui five column grid'>
+        Your Bot Army
+        <div className='row bot-army-row'>
           {/*...and here...*/}
-          {armyItem}
+
+          {mappedBotArmy}
         </div>
       </div>
     </div>
